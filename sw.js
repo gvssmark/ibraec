5ae96f109cb5f79aecaee4e33405a6f820b6c7cb
gvssmark <83894487+gvssmark@users.noreply.github.com>
2022-09-09 12:06:28 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([ 
+      'index.js',
+      'index.html',
+      'images/ibra192.png',
+      'images/ibra512.png',
+            ])),
+  );
+});
+
+
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
